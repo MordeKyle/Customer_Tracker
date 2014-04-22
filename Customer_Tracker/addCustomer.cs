d7ab4007e2b963906165d1551037a58ea83c2153
MordeKyle <Kyle@MordeKyle.com>
2014-04-22 12:02:15 -0500

    Version: 1.0 Optimistic Robin
    04/22/2014 @ 11:56
    •Created Repository and Second Form (addCustomer)
    -Initial Commit

---
+++ Customer_Tracker/addCustomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*Program: Customer Tracker
+  Author: MordeKyle (Kyle McBride)
+  Date: 04/22/2014
+  Description: This program will keep track of a customer's purchase history and personal information.
+                                          ***SEE CHANGELOG IN SOLUTION DIRECTORY***				*/
+
+namespace Customer_Tracker
+{
+    public partial class addCustomer : Form
+    {
+        public addCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
